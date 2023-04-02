ceab7fe4d0684c6141a2beced15346accdfefcd6
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-02 17:36:30 -0400

    fixed :)

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const PORT = process.env.PORT || 4200;
+
 app.use(express.static(__dirname + '/dist/my-golfr'));
 
-app.listen(process.env.PORT || 8000);
-
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/my-golfr/'));
+  res.sendFile(path.join(__dirname + '/dist/my-golfr/index.html'));
 });
+
+app.listen(PORT, console.log(`Server Running on port ${PORT}`));
