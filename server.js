f5c292de8be902f279304a5b161d42be86477772
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-02 16:21:55 -0400

    fixed serve on server

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname + '/dist/my-golfr'));
 app.listen(process.env.PORT || 8000);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/my-golfr/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
