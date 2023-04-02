abc967813490084ab01996b5771fb2fb97cebdc2
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-02 02:54:14 -0400

    changed root url

--- src/app/utilities/enviroment.ts
+++ src/app/utilities/enviroment.ts
@@ -1,3 +1,2 @@
 export const apiKey = 'AIzaSyDeza5bf-2ecSNe0i7QeDVDUnLHsDk7xoU';
-// export const ROOT_URL = 'http://ec2-3-145-45-202.us-east-2.compute.amazonaws.com:3000/';
-export const ROOT_URL = 'http://localhost:3000/';
+export const ROOT_URL = 'https://mygolfr-api.onrender.com';
