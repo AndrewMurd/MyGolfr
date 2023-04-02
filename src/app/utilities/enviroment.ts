0ebb40e5083b5c414f38db609074faa02cf48f4c
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-02 02:01:52 -0400

    added enviroment variables for frontend

---
+++ src/app/utilities/enviroment.ts
@@ -0,0 +1,3 @@
+export const apiKey = 'AIzaSyDeza5bf-2ecSNe0i7QeDVDUnLHsDk7xoU';
+// export const ROOT_URL = 'http://ec2-3-145-45-202.us-east-2.compute.amazonaws.com:3000/';
+export const ROOT_URL = 'http://localhost:3000/';
