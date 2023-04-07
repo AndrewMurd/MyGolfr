4adf000815268192407330f85b34638c444ea8c7
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-07 01:42:18 -0400

    updated origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,6 +2,10 @@ const allowedOrigins = [
   'http://localhost:4200',
   'http://localhost:3000',
   'https://mygolfr.onrender.com',
+  'http://ec2-18-118-12-56.us-east-2.compute.amazonaws.com:3000',
+  'http://ec2-18-118-12-56.us-east-2.compute.amazonaws.com',
+  'http://18.118.12.56:3000',
+  'http://18.118.12.56',
 ]
 
 module.exports = allowedOrigins
