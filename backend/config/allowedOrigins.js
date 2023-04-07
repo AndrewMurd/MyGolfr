2a747389873fffb3120a007a925f40feeebc15b3
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-07 15:23:02 -0400

    origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,12 +2,7 @@ const allowedOrigins = [
   "http://localhost:4200",
   "http://localhost:3000",
   "https://mygolfr.onrender.com",
-  "http://ec2-18-118-12-56.us-east-2.compute.amazonaws.com:3000",
-  "http://ec2-18-118-12-56.us-east-2.compute.amazonaws.com",
-  "http://18.118.12.56:3000",
-  "http://18.118.12.56",
-  "http://*",
-  "https://*",
+  "http://ec2-18-220-49-19.us-east-2.compute.amazonaws.com:3000/",
 ];
 
 module.exports = allowedOrigins;
