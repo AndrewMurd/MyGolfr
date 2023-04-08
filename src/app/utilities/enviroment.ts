ea7696875684062b72aa63f453c61e5b27f0eb6c
AndrewMurd <andrew.murdoch@ontariotechu.net>
2023-04-07 20:51:26 -0400

    meh

--- src/app/utilities/enviroment.ts
+++ src/app/utilities/enviroment.ts
@@ -1,4 +1,4 @@
 export const apiKey = 'AIzaSyDeza5bf-2ecSNe0i7QeDVDUnLHsDk7xoU';
-export const ROOT_URL = 'http://ec2-18-220-49-19.us-east-2.compute.amazonaws.com/backend/';
+export const ROOT_URL = 'http://ec2-18-220-49-19.us-east-2.compute.amazonaws.com:3000/backend/';
 // export const ROOT_URL = 'https://mygolfr.onrender.com/backend/';
 // export const ROOT_URL = 'http://localhost:3000/backend/';
